b09cab802026add673a27b1ff5233353ab2d3078
Imanalopher <iman.mamadiyev@gmail.com>
2017-12-25 18:44:16 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+
+    title = 'Welcome';
+    users: User[];
+    selectedUser: User;
+
+    constructor(private userService: UserService){}
+
+    ngOnInit() {
+        this.getUsers();
+    }
+
+    getUser(id: number){
+        this.userService.getUser(id).subscribe(user => this.selectedUser = user);
+    }
+
+    getUsers(){
+        this.userService.getUsers().subscribe(users => this.users = users);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
-
+import { UserService } from './user.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [ UserService ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
